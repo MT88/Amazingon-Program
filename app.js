489da92abd73384d9b8401c77151b293de8d7fc3
Michael Traher <matraher1@gmail.com>
2018-08-23 19:10:18 -0400

    Program wish list

---
+++ app.js
@@ -0,0 +1,37 @@
+/*  The goal of this application, and there may need to be several applications,
+is to condense all of my business operations for E-Commerce all
+into one place.*/
+
+//  List of operations that currently take place for business are as follows:
+
+//  Sourcing and Ordering of Products
+
+//  Listing/Uploading of products on online stores (Amazon/Ebay/Hopefully my own site)
+
+//  Inventory tracking for Amazon with Inventory Lab. Ebay has an Excel sheet.
+
+//  Creating of shipments for Amazon. Inventory Lab generates a file and uploads it for Amazon.
+
+/*  ACCOUNTING: Inventory Lab also does the accounting.
+The program tracks expenses and income for tax purposes.
+Tax Liabilities are accounted for in Inventory Lab.
+With Nexus Law changes it will be necessary to more closely track both quantity of sales
+in all 50 states and the ammount of revenue produced from all 50 states in real time.
+Accounting for Ebay is only done on an Excel sheet. You can imagine how bad it would
+be for my company to cross one of the Tax Liability thresholds on Ebay and be in
+very big trouble for not filing taxes like I was supposed to.
+*/
+
+/*  Marketing:  I would like to have an in house marketing setup. I would like to be able to generate
+A/B split test lead pages and an Email auto responder to keep customers informed about the business.
+In doing this I accomplish the task of at least three different applications and I can offer marketing
+services to other businesses as well. Note; this may be part of the overall website that gets created.
+*/
+
+/*  Systematic Liquidation Sales: I would like to liquidate all assets that have not sold
+or are not selling after 3 months automatically. This would look like an automatic, constantly
+running liquidation sale.
+*/
+
+/*  Last but not least, Displaying of products on an independent website where they can be
+browsed and purchased directly.*/
